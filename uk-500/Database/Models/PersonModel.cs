086bfcc80a3d0bb5783c9235893188389fc07ab1
Måns Isaksson <mans.95.isaksson@gmail.com>
2019-07-17 20:57:19 +0200

    Updated Dabase rows

--- uk-500/Database/Models/PersonModel.cs
+++ uk-500/Database/Models/PersonModel.cs
@@ -32,8 +32,8 @@ namespace uk_500.Database
         public string last_name { get; set; }
         public string company_name { get; set; }
         public string address { get; set; }
-        //public string city { get; set; }; I do not need these as I've got the postcode, it's also something I don't want the user to edit
-        //public string county { get; set; };
+        public string city { get; set; }
+        public string county { get; set; }
         public string postal { get; set; }
         public string phone1 { get; set; }
         public string phone2 { get; set; }
